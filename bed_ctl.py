c2afe0b55c5048928ca35ae31591cb0fd51189c0
Jeramy Barrett <jeramy.barrett@gmail.com>
2020-10-12 23:10:41 -0400

    added all gpio and assoc with canvas

--- bed_ctl.py
+++ bed_ctl.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-#from flask import Flask
-#app = Flask(__name__)
-
-
-#@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
-
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=80)
-#    print("Running")
-
 from bottle import route, run, template, static_file
 from gpiozero import LED
 
@@ -21,19 +9,64 @@ from gpiozero import LED
 def index():
     return static_file('index.htm', root='/home/pi/bed_ctl')
 
-@route('/headUpEn')
-def headUpEn():
-    headUp.on()
-
+@route('/headUpOn')
+def headUpOn():
+    headUpGPIO_A.on()
+    headUpGPIO_B.on()
     return
 
-@route('/headUpDis')
-def headUpDis():
-    headUp.off()
-
+@route('/headUpOff')
+def headUpOff():
+    headUpGPIO_A.off()
+    headUpGPIO_B.off()
     return
 
-headUp = LED(17)
+@route('/headDownOn')
+def headDownOn():
+    headDownGPIO_A.on()
+    headDownGPIO_B.on()
+    return
+
+@route('/headDownOff')
+def headDownOff():
+    headDownGPIO_A.off()
+    headDownGPIO_B.off()
+    return
+
+@route('/footUpOn')
+def footUpOn():
+    footUpGPIO_A.on()
+    footUpGPIO_B.on()
+    return
+
+@route('/footUpOff')
+def footUpOff():
+    footUpGPIO_A.off()
+    footUpGPIO_B.off()
+    return
+
+@route('/footDownOn')
+def headUpfootDownOnDis():
+    footDownGPIO_A.on()
+    footDownGPIO_B.on()
+    return
+
+@route('/footDownOff')
+def footDownOff():
+    footDownGPIO_A.off()
+    footDownGPIO_B.off()
+    return
+
+
+headUpGPIO_A = LED(17)
+headDownGPIO_A = LED(27)
+footUpGPIO_A = LED(4)
+footDownGPIO_A = LED(18)
+
+headUpGPIO_B = LED(6)
+headDownGPIO_B = LED(16)
+footUpGPIO_B = LED(8)
+footDownGPIO_B = LED(12)
 
 
 run(host='0.0.0.0', port=80)
