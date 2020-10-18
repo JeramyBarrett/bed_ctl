019e7da84cec3a5aff2d85d0ff96c725abb30d50
Jeramy Barrett <jeramy.barrett@gmail.com>
2020-10-17 23:30:09 -0400

    wackey edge detection workaround new state machine for edge detection all down works

--- bed_ctl.py
+++ bed_ctl.py
@@ -7,24 +7,29 @@ from threading import Thread
 from time import sleep
 
 class motorCtl:
-    class state(Enum):
-        movingUp = 1
-        movingDown = 2
+    class buttonState(Enum):
+        up = 1
+        down = 2
         idle = 3
 
-    def __init__(self, pinUp, pinDown, pinPosition):
+    class motorState(Enum):
+        idle = 1
+        buttonInput = 2
+        awatingEdge = 3
+        edge = 4
+
+    def __init__(self, pinUp, pinDown, pinEncoder):
+        self._encoderPosition = 0
+        self._buttonState = self.buttonState.idle
+        self._motorState = self.motorState.idle
+
+        # __io = DigitalInputDevice(pinEncoder, pull_up=True)
+        # __io.when_activated = self._encoderEdge
         self._moveUp = DigitalOutputDevice(pinUp)
         self._moveDown = DigitalOutputDevice(pinDown)
-        positionInput = DigitalInputDevice(pinPosition, pull_up=True)
-        positionInput.when_activated = self._positionEdge
 
         self._moveUp.off()
         self._moveDown.off()
-
-        self._inMotion = False
-        self._edgeDetected = False
-        self._position = 0
-        self._state = self.state.idle
 
         self._motionDetectThread = Thread(target=self._motionDetect, args=())
         self._motionDetectThread.daemon = True
@@ -34,66 +39,72 @@ class motorCtl:
         pass
 
     def upStart(self):
-        if self._state != self.state.idle:
+        if self._buttonState != self.buttonState.idle:
             return
         
-        self._state = self.state.movingUp
+        self._buttonState = self.buttonState.up
         self._moveUp.on()
+        self._motorState = self.motorState.buttonInput
 
     def upStop(self):
-        if self._state != self.state.movingUp:
+        if self._buttonState != self.buttonState.up:
             return
 
-        self._state = self.state.idle
+        self._buttonState = self.buttonState.idle
         self._moveUp.off()
 
     def downStart(self):
-        if self._state != self.state.idle:
+        if self._buttonState != self.buttonState.idle:
             return
         
-        self._state = self.state.movingDown
+        self._buttonState = self.buttonState.down
         self._moveDown.on()
+        self._motorState = self.motorState.buttonInput
 
     def downStop(self):
-        if self._state != self.state.movingDown:
+        if self._buttonState != self.buttonState.down:
             return
 
-        self._state = self.state.idle
+        self._buttonState = self.buttonState.idle
         self._moveDown.off()
 
     def getPosition(self):
-        return self._position
+        return self._encoderPosition
 
     def _motionDetect(self):
         while True:
-            # Still in motion
-            if self._inMotion == True & self._edgeDetected == True:
-                self._edgeDetected = False
+            if self._motorState == self.motorState.idle:
+                continue
 
-            # Just started moving
-            elif self._inMotion == False & self._edgeDetected == True:
-                self._inMotion = True
-                self._edgeDetected = False
+            elif self._motorState == self.motorState.buttonInput:
+                self._motorState = self.motorState.awatingEdge
 
-            # Stopped moving
-            elif self._inMotion == True & self._edgeDetected == False:
-                self._inMotion = False
+            elif self._motorState == self.motorState.awatingEdge:
+                if self._buttonState == self.buttonState.down:
+                    print("Down Stopped")
+                    self.downStop()
 
-            sleep(0.5)
+                if self._buttonState == self.buttonState.up:
+                    print("Up Stopped")
+                    self.upStop()
 
-    def _positionEdge(self):
-        print("Edge Detected")
-        if self._state == self.state.movingUp:
-            self._position += 1
-        elif self._state == self.state.movingDown & self._position > 0:
-            self._position -= 1
+                self._motorState = self.motorState.idle
 
-        self._edgeDetected = True
-        self._inMotion = True
+            elif self._motorState == self.motorState.edge:
+                self._motorState = self.motorState.awatingEdge
+
+            sleep(1)
+
+    def _encoderEdge(self):
+        if self._buttonState == self.buttonState.up:
+            self._encoderPosition += 1
+        elif (self._buttonState == self.buttonState.down) & (self._encoderPosition > 0):
+            self._encoderPosition -= 1
+
+        self._motorState = self.motorState.edge
 
     def inMotion(self):
-        return self._inMotion
-
+        return False
 
 @route('/')
 def index():
@@ -147,15 +158,48 @@ def footDownStop():
     leftFoot.downStop()
     return
 
+@route('/allDownRun')
+def allDownRun():
+    rightHead.downStart()
+    rightFoot.downStart()
+    leftHead.downStart()
+    leftFoot.downStart()
+    return
+
 positionFormat = "<p style=\"text-align: center; font-size: xx-large\">{{val}}</p>"
 
 @route('/headPos')
 def headPos():
-    return template(positionFormat, val=rightHead.getPosition())
+    return template(positionFormat, val=leftHead.getPosition())
 
 @route('/footPos')
 def footPos():
-    return template(positionFormat, val=rightFoot.getPosition())
+    return template(positionFormat, val=leftFoot.getPosition())
+
+def _leftHeadEdge():
+    # print("Left Head Edge")
+    leftHead._encoderEdge()
+
+def _leftFootEdge():
+    # print("Left Foot Edge")
+    leftFoot._encoderEdge()
+
+def _rightHeadEdge():
+    # print("Right Head Edge")
+    rightHead._encoderEdge()
+
+def _rightFootEdge():
+    # print("Right Foot Edge")
+    rightFoot._encoderEdge()
+
+lh = DigitalInputDevice(14, pull_up=True)
+lh.when_activated = _leftHeadEdge
+lf = DigitalInputDevice(15, pull_up=True)
+lf.when_activated = _leftFootEdge
+rh = DigitalInputDevice(7, pull_up=True)
+rh.when_activated = _rightHeadEdge
+rf = DigitalInputDevice(5, pull_up=True)
+rf.when_activated = _rightFootEdge
 
 leftHead = motorCtl(17, 27, 14)
 leftFoot = motorCtl(4, 18, 15)
